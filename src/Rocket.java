78c6bd041f8f2a358c474907ea8b6ab2eaebd0b1
Herman99 <levyinathan@gmail.com>
2016-07-08 14:34:12 -0500

    Rocket

--- src/Rocket.java
+++ src/Rocket.java
@@ -2,21 +2,29 @@ import acm.program.*;
 import acm.graphics.*;
 import java.awt.Color;
 
-
 public class Rocket extends GPolygon
 { 
-GPolygon gpoly = new GPolygon();
+    GPolygon gpoly = new GPolygon();
     private double x;
     private double y;
     private Space mySpace;
 
-    public Rocket( Color c)
+    public Rocket(Color c)
     {
-        
         setColor(c); 
         setFillColor(c);
+        setFilled(true);
     }
-    
 
+    public void setX (double x) 
+    {
+        setLocation(x, getY());
 
+    }
+
+    public void setY (double y)
+    {
+        setLocation(getX(), y);
+
+    }
 }
--- src/Space.java
+++ src/Space.java
@@ -10,20 +10,52 @@ public class Space extends GraphicsProgram
     public void init()
     {
         Color c = new Color((int)(256 * Math.random()), (int)(256 * Math.random()), (int)(256 * Math.random()));
-        
+
         myRocket = new Rocket(c);
-        
+
         myRocket.addVertex(getWidth()/2, getHeight()/2);
-        myRocket.addVertex(getWidth()/2 + 20, getHeight()/2 - 25);
-        myRocket.addVertex(getWidth()/2 + 20, getHeight()/2 + 25);
-        myRocket.addVertex(getWidth()/2 - 20, getHeight()/2 - 25);
-        myRocket.addVertex(getWidth()/2 - 20 ,getHeight()/2 + 25);
-        
+        myRocket.addVertex(getWidth()/2 + 14, getHeight()/2 - 21);
+        myRocket.addVertex(getWidth()/2 + 14, getHeight()/2 + 21);
+        myRocket.addVertex(getWidth()/2 - 14, getHeight()/2 - 21);
+        myRocket.addVertex(getWidth()/2 - 14 ,getHeight()/2 + 21);
+
         add(myRocket);
 
-        
+        setBackground(Color.BLACK);
+
+        addMouseListeners();
+
+    }
+    
+    public void mouseMoved(MouseEvent e)
+    {
+        double y;
+
+        y = e.getY();
+
+        if (y < 0)
+            y = 0;
+
+        if (y > getHeight() - 50)
+            y = getHeight() - 50;
+
+        myRocket.setY(y);
+        myRocket.setY(y);
+
+        double x;
+        x = e.getX();
+        if (x<0)
+            x = 0;
+
+        if (x> getWidth() - 60)
+            x= getWidth() - 60;
+
+        myRocket.setX(x);
+        myRocket.setX(x);
     }
 
+    
+    
     public Rocket getmyRocket(){
         return myRocket;
     }
